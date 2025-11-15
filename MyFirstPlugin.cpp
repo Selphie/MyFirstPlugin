601659e8daaf7ca21316c0d52e2200d31504f20a
My Fi <My.Fi@gmx.de>
2025-11-15 16:13:22 +0100

    hallo

---
+++ MyFirstPlugin.cpp
@@ -0,0 +1,47 @@
+#include "MyFirstPlugin.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControls.h"
+
+MyFirstPlugin::MyFirstPlugin(const InstanceInfo& info) // Konstruktor
+: iplug::Plugin(info, MakeConfig(kNumParams, kNumPresets)) // legt fest, wie viele Parameter und Presets es gibt
+{
+  GetParam(kGain)->InitDouble("Gain", 0., 0., 100.0, 0.01, "%"); // Initialisiert einen Parameter vom Typ Double
+  GetParam(kDrive)->InitDouble("Drive", 0., 0., 100.0, 0.01, "%"); // Initialisiert einen Parameter vom Typ Double
+
+
+#if IPLUG_EDITOR // http://bit.ly/2S64BDd
+  mMakeGraphicsFunc = [&]() { // erstellt das Fenster und das Canvas
+    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT));
+  };
+  
+  mLayoutFunc = [&](IGraphics* pGraphics) { // definiert alle Controls und Layouts in der GUI
+    pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
+    pGraphics->AttachPanelBackground(COLOR_GRAY);
+    pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
+    const IRECT b = pGraphics->GetBounds();
+    pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "Hello iPlug 2!", IText(50)));
+    pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain, "Gain"));
+    pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(100), kDrive, "Drive"));
+  };
+#endif
+}
+
+#if IPLUG_DSP
+void MyFirstPlugin::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+  const double gain = GetParam(kGain)->Value() / 100.;
+  const double drive = GetParam(kDrive)->Value() / 100.;
+  const int nChans = NOutChansConnected();
+  
+   for (int s = 0; s < nFrames; s++)
+  {                                  // nFrames beschriebt die Anzahl Samples im aktuellen Audio-Block
+    for (int c = 0; c < nChans; c++) // nChans beschreibt die Anzahl der Kanäle
+    {
+      double sample = inputs[c][s];
+      sample *= gain;  // zuerst Gain
+      sample *= drive; // dann Drive (oder deine Distortion-Funktion)
+      outputs[c][s] = sample;
+    }
+  }
+}
+#endif
---
+++ MyFirstPlugin.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "IPlug_include_in_plug_hdr.h"
+
+const int kNumPresets = 1;
+
+enum EParams
+{
+  kGain = 0,
+  kDrive = 1,
+  kNumParams
+};
+
+using namespace iplug;
+using namespace igraphics;
+
+class MyFirstPlugin final : public Plugin
+{
+public:
+  MyFirstPlugin(const InstanceInfo& info);
+
+#if IPLUG_DSP // http://bit.ly/2S64BDd
+  void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+#endif
+};
